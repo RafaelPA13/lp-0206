51ddc5de15e431cadfa60c2dd3bda057b3e0c5f6
RafaelPA13 <84384192+RafaelPA13@users.noreply.github.com>
2022-06-02 08:46:30 -0300

    Add files via upload

---
+++ routes/login.js
@@ -0,0 +1,30 @@
+module.exports = (app)=>{
+    //abrir o arquivo login.js
+    app.get('/login',(req,res)=>{
+        res.render('login.ejs')
+    })
+
+    //validar o usuário e senha
+    app.post('/login', async(req,res)=>{
+        //recuperar as informações digitadas no formulário
+        var dados = req.body
+        //conectar com o banco de dados
+        var database = require('../config/database')()
+        //selecionar a model usuarios
+        var usuarios = require('../models/usuarios')
+        //verificar se o email está cadastrado
+        var verificar = await usuarios.findOne({email:dados.email})
+        if(!verificar){
+            return res.send('Email não Cadastrado!')
+        }
+        var cript = require('bcryptjs')
+        var comparar = await cript.compare(dados.senha,verificar.senha)
+        if(!comparar){
+            return res.send('Senha Invalida!')
+        }
+        //buscar as atividades do usuário
+        const atividades = require("../models/atividades")
+        var buscar = await atividades.find({usuario:verificar._id})
+        res.render('atividades.ejs',{nome:verificar.nome,id:verificar._id,lista:buscar})
+    })
+}
